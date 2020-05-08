6ca8c810a2a82c491592bac492a2791733839242
chang180 <chang180@gmail.com>
2020-05-08 14:23:47 +0800

    建立輸入發票的畫面及流程

---
+++ save_invoice.php
@@ -0,0 +1,8 @@
+<?php
+echo "period=",$_POST['period'],"<br>";
+echo "year=",$_POST['year'],"<br>";
+echo "code=",$_POST['code'],"<br>";
+echo "number=",$_POST['number'],"<br>";
+echo "expense=",$_POST['expense'],"<br>";
+
+?>
