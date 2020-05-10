4030defb6ffa3db0b3915b0b6c9e34955b77beb4
張 建文 <chang180@gmail.com>
2020-05-10 09:42:52 +0800

    輸入獎號表單外觀美化完成

--- index.php
+++ index.php
@@ -22,7 +22,7 @@
     <?php include "./include/header.php"; ?>
     <div class="container-fluid mt-5 p-3 col col-3 d-flex justify-content-center border rounded-lg shadow bg-dark alert-light">
         <form action="save_invoice.php" method="post">
-            <div class="input-group row mb-3 p-2">
+            <div class="input-group form-row mb-3 p-2">
                 <label class="col-3" for="year"> 年份：</label>
                 <select class="form-control col-9" name="year">
                     <option value="2020">2020</option>
@@ -30,7 +30,7 @@
                     <option value="2022">2022</option>
                 </select>
             </div>
-            <div class="input-group row mb-3 p-2">
+            <div class="input-group form-row mb-3 p-2">
                 <label class="col-3" for="period">期別：</label>
                 <select class="form-control col-9" name="period">
                     <option value="1">1,2月</option>
@@ -41,19 +41,22 @@
                     <option value="6">11,12月</option>
                 </select>
             </div>
-            <div class="input-group mb-3 p-2 row">
-                <label class="form-label col-3">獎號：</label>
+            <div class="input-group mb-3 p-2 form-row">
+                <label class="form-label col-3" for="number">獎號：</label>
                 <!-- 規定輸入必須為2個大寫英文字母，自動轉為大寫 -->
                 <input class="form-control col-2 text-uppercase" type="text" name="code" pattern="[a-zA-Z]{2}" placeholder="AA" maxlength="2">
                 <input class="form-control col-7" type="text" name="number" pattern="\d{8}" placeholder="88888888" maxlength="8">
             </div>
-            <div class="input-group mb-3 p-2 row">
+            <div class="input-group mb-3 p-2 form-row">
                 <label class="form-label col-3" for="expense">花費：</label>
                 <input class="form-control col-9" type="text" name="expense" pattern="^[1-9]\d*$" maxlength="10">
             </div>
                 <input class="btn btn-outline-info m-2" type="submit" valut="儲存">
         </form>
     </div>
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 </body>
 
 </html>
--- list.php
+++ list.php
@@ -1,12 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>統一發票管理系統</title>
     <link rel="stylesheet" href="./css/style.css">
+    <style>
+        body {
+            min-width: 600px;
+            min-height: 100vh;
+            overflow: hidden;
+        }
+    </style>
 </head>
+
 <body>
-<?php include "./include/header.php"    ;?>    
+    <?php include "./include/header.php"; ?>
+    
 </body>
+
 </html>
