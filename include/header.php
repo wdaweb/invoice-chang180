bc81407788e226f13840eba04536b8a942a8fa3b
張 建文 <chang180@gmail.com>
2020-05-31 22:33:30 +0800

    header modified

--- include/header.php
+++ include/header.php
@@ -1,7 +1,7 @@
     <nav class="container nav justify-content-center">
-    <li class="nav-item"><a class="nav-link" href="list.php">發票列表</a></li>
-    <li class="nav-item"><a class="nav-link" href="invoice.php">各期獎號</a></li>
-    <li class="nav-item"><a class="nav-link" href="add_invoice.php">新增中獎號碼</a></li>
+    <li class="nav-item"><a class="nav-link btn-lg btn-primary m-1" href="list.php">發票列表</a></li>
+    <li class="nav-item"><a class="nav-link btn-lg btn-primary m-1" href="invoice.php">各期獎號</a></li>
+    <li class="nav-item"><a class="nav-link btn-lg btn-primary m-1" href="add_invoice.php">新增中獎號碼</a></li>
     <!-- <li class="nav-item"><a class="nav-link" href="award.php">對獎</a></li> -->
-    <li class="nav-item"><a class="nav-link" href="index.php">輸入發票</a></li>
+    <li class="nav-item"><a class="nav-link btn-lg btn-primary m-1" href="index.php">輸入發票</a></li>
     </nav>
